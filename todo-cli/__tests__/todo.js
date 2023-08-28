9329c11da2a3444a80b54d517a1cbc96a011fb1b
Gowtham-S-Git <s.gowtham.engr@gmail.com>
2023-08-28 10:16:05 +0530

    Initial Commit

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,59 +1,21 @@
+// __tests__/todo.js
 /* eslint-disable no-undef */
-let todoList = require("../todo");
-const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
-describe("Todo test cases", () => {
-  beforeAll(() => {
-    const today = new Date();
-    const oneDay = 60 * 60 * 24 * 1000;
-    [
-      {
-        title: "Complete assignment",
-        completed: false,
-        dueDate: new Date(today.getTime() - 1 * oneDay).toLocaleDateString(
-          "en-CA"
-        ),
-      },
-      {
-        title: "Go for shopping",
-        completed: false,
-        dueDate: new Date().toLocaleDateString("en-CA"),
-      },
-      {
-        title: "Complete project",
-        completed: false,
-        dueDate: new Date(today.getTime() + 1 * oneDay).toLocaleDateString(
-          "en-CA"
-        ),
-      },
-    ].forEach(add);
-  });
-  test("Add new todo", () => {
-    expect(all.length).toEqual(3);
+// eslint-disable-next-line quotes
+const db = require("../models");
 
-    add({
-      title: "Take the test",
+describe('Todolist Test Suite', () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
+
+  test('Should add new todo', async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
+      title: 'Test_todo',
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
+      dueDate: new Date(),
     });
-
-    expect(all.length).toEqual(4);
-  });
-
-  test("Todo mark as complete", () => {
-    expect(all[0].completed).toEqual(false);
-    markAsComplete(0);
-    expect(all[0].completed).toEqual(true);
-  });
-
-  test("Test for overdue", () => {
-    expect(overdue().length).toEqual(1);
-  });
-
-  test("Test due today", () => {
-    expect(dueToday().length).toEqual(2);
-  });
-
-  test("Test for due later", () => {
-    expect(dueLater().length).toEqual(1);
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
   });
 });
---
+++ todo-cli/listTodos.js
@@ -0,0 +1,14 @@
+/* eslint-disable quotes */
+//  listTodos.js
+const db = require("./models/index");
+
+const listTodo = async () => {
+  try {
+    await db.Todo.showList();
+  } catch (error) {
+    console.error(error);
+  }
+};
+(async () => {
+  await listTodo();
+})();
